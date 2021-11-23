cfb8611fc9e8208c37aa62f54001adff6c87e338
k-szulc <kamil.szulc@hotmail.com>
2021-11-23 09:44:44 +0100

    added CircleCI badge

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![CircleCI](https://circleci.com/gh/k-szulc/recipes/tree/main.svg?style=svg)](https://circleci.com/gh/k-szulc/recipes/tree/main)
 # recipes
+Recipes java project based on SFG course
--- src/main/java/xyz/itbs/recipes/RecipesApplication.java
+++ src/main/java/xyz/itbs/recipes/RecipesApplication.java
@@ -1,21 +1,14 @@
 package xyz.itbs.recipes;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RecipesApplication {
 
-//	private static final Logger logger = LoggerFactory.getLogger(RecipesApplication.class);
-
 	public static void main(String[] args) {
 
 		SpringApplication.run(RecipesApplication.class, args);
-
-//		logger.error("Error message !!");
-//		logger.info("Info message");
 
 	}
 
